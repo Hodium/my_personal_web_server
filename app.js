7758960c0b8514725c20eac089ad854863920780
QXZ1XIZ <pedro.diogo.oliveira@criticaltechworks.com>
2023-01-18 23:10:24 +0000

    added token to protect endpoint

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
+const jwt = require('jsonwebtoken');
 
 const path = 'public';
 const path2 = 'backoffice';
@@ -12,28 +13,58 @@ app.use('/', express.static(path));
 app.use('/backoffice', express.static(path2));
 
 app.use(cors({ origin: '*' }));
+jwt.sign({gajo :'nome'},'teste123',(err,token)=>{
+  res.json({
+    token
+  })
+})
+app.get('/test',verifyToken, function (req, res) {
+  jwt.verify(req.token,'teste123',(err,authData)=>{
+    if(err)
+      res.send("403 - TENHO TOKEN MAS NÃO É ESTE!");
+    else{
+      res.send("200 - TENHO TOKEN E É ESTE!!");
+    }
+  })
+});
 
-// app.get('/test', function (req, res) {
-//   res.send("1000!");
-// });
-//
-// app.get('/anotherTest', function (req, res) {
-//   res.send("2000!");
-// });
+//Verify Token
+function verifyToken(req,res,next){
+  //Auth header value = > send token into header
+  const bearerHeader = req.headers['authorization'];
+  //check if bearer is undefined
+  if(typeof bearerHeader !== 'undefined'){
+    //split the space at the bearer
+    const bearer = bearerHeader.split(' ');
+    //Get token from string
+    const bearerToken = bearer[1];
+    //set the token
+    req.token = bearerToken;
+    //next middleweare
+    next();
+  }else{
+    //Fobidden
+    res.send("403 - NÃO TENHO TOKEN");
+  }
+}
 
-// app.post('/receive', function (request, respond) {
-//   var body = '';
-//   filePath = path3 + '/data.txt';
-//   request.on('data', function (data) {
-//     body += data;
-//   });
-//
-//   request.on('end', function () {
-//     fs.appendFile(filePath, body, function () {
-//       respond.send(body).end();
-//     });
-//   });
-// });
+app.get('/anotherTest', function (req, res) {
+  res.send("2000!");
+});
+
+app.post('/receive', function (request, respond) {
+  var body = '';
+  filePath = path3 + '/data.txt';
+  request.on('data', function (data) {
+    body += data;
+  });
+
+  request.on('end', function () {
+    fs.appendFile(filePath, body, function () {
+      respond.send(body).end();
+    });
+  });
+});
 
 // app.delete('/delete', function (request, respond) {
 //   filePath = path3 + '/data.txt';
