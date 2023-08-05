b871bcc3e6bc4cfb243c45930eeac6b474a97ea3
QXZ1XIZ <pedro.diogo.oliveira@criticaltechworks.com>
2023-08-05 09:39:31 +0100

    reset status to basic node serve

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# my_personal_web_server
+# basic_node_server
+
+- When using CPanel:
+1. Install "vue" next to the server folder
+2. Add "public", "backoffice" and "docs" folders inside the server folder  
--- app.js
+++ app.js
@@ -13,39 +13,39 @@ app.use('/backoffice', express.static(path2));
 
 app.use(cors({ origin: '*' }));
 
-// app.get('/test', function (req, res) {
-//   res.send("1000!");
-// });
-//
-// app.get('/anotherTest', function (req, res) {
-//   res.send("2000!");
-// });
+app.get('/test', function (req, res) {
+  res.send("1000!");
+});
 
-// app.post('/receive', function (request, respond) {
-//   var body = '';
-//   filePath = path3 + '/data.txt';
-//   request.on('data', function (data) {
-//     body += data;
-//   });
-//
-//   request.on('end', function () {
-//     fs.appendFile(filePath, body, function () {
-//       respond.send(body).end();
-//     });
-//   });
-// });
+app.get('/anotherTest', function (req, res) {
+  res.send("2000!");
+});
 
-// app.delete('/delete', function (request, respond) {
-//   filePath = path3 + '/data.txt';
-//   fs.unlink(filePath, (err) => {
-//     if (err) {
-//       console.error(err)
-//       return
-//     }
-//     respond.end()
-//     //file removed
-//   })
-// })
+app.post('/receive', function (request, respond) {
+  var body = '';
+  filePath = path3 + '/data.txt';
+  request.on('data', function (data) {
+    body += data;
+  });
+
+  request.on('end', function () {
+    fs.appendFile(filePath, body, function () {
+      respond.send(body).end();
+    });
+  });
+});
+
+app.delete('/delete', function (request, respond) {
+  filePath = path3 + '/data.txt';
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    respond.end()
+    //file removed
+  })
+})
 
 // set port, listen for requests
 const PORT = 80;
